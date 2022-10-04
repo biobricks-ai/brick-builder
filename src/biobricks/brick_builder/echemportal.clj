fb2c6766f154d8913707ddfe159ffae22558f8c0
John Shaffer <john@insilica.co>
2022-10-04 16:25:23 -0500

    Implement automated crawling of echemportal

--- src/biobricks/brick_builder/echemportal.clj
+++ src/biobricks/brick_builder/echemportal.clj
@@ -5,29 +5,60 @@
 
 (def substance-search-uri "https://www.echemportal.org/echemportal/substance-search")
 
+(defn click-csv-button? [driver]
+  (try
+    (ea/click-visible
+     driver
+     [{:tag :echem-export-buttons}
+      {:tag :button}
+      {:fn/text "CSV"}])
+    true
+    (catch Exception e
+      (if (ea/exists? driver {:fn/text "Your search criteria didn't match any substance."})
+        false
+        (throw e)))))
+
 (defn substance-search-results [download-dir search-term]
   (let [uri (str substance-search-uri "?"
                  (uri/map->query-string {:query_term search-term}))
         opts {:download-dir download-dir
+              :headless true
               :user-agent "biobricks.ai brick builder"}]
     (ea/with-wait-timeout 30
       (ea/with-driver :chrome opts driver
-        (doto driver
-          (ea/go uri)
-          (ea/click-visible [{:tag :echem-export-buttons}
-                             {:tag :button}
-                             {:fn/text "CSV"}]))
-        (loop [[i & more] (range 60)]
-          (let [results-file (->> (fs/list-dir download-dir)
-                                  (filter #(= "result.csv" (fs/file-name %)))
-                                  first)]
-            (or
-             results-file
-             (when more
-               (Thread/sleep 500)
-               (recur more)))))))))
+        (ea/go driver uri)
+        (when (click-csv-button? driver)
+          (loop [[i & more] (range 60)]
+            (let [results-file (->> (fs/list-dir download-dir)
+                                    (filter #(= "result.csv" (fs/file-name %)))
+                                    first)]
+              (or
+               results-file
+               (when more
+                 (Thread/sleep 500)
+                 (recur more))))))))))
+
+(defn crawl-substances []
+  (let [target-dir (fs/path "target" "echemportal")]
+    (fs/create-dirs target-dir)
+    (doseq [i (range 10 9999999)]
+      (fs/with-temp-dir [dir {:prefix "brick-builder"}]
+        (if-let [results (substance-search-results dir (str i "-*-*"))]
+          (do
+            (fs/move results (fs/path target-dir (str i ".csv")))
+            (prn "echemportal: Saved results for" i))
+          (prn "echemportal: No results for" i))))))
 
 (comment
+  ;; Download search results
   (fs/with-temp-dir [dir {:prefix "brick-builder"}]
     (-> (substance-search-results (str dir) "108-*-*")
-        fs/file slurp)))
+        fs/file slurp))
+  
+  ;; Empty results return nil
+  (fs/with-temp-dir [dir {:prefix "brick-builder"}]
+    (substance-search-results (str dir) "05-*-*"))
+  
+  ;; Run crawler
+  (crawl-substances)
+  )
